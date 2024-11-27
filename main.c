1a52ea8e7156508595318d3ac08cc39059a26e0d
Georgi Stoyanov <153766792+georgi-stoyanov-jp@users.noreply.github.com>
2024-11-27 15:11:24 +0200

    Update main.c
    Refactored code for better readability: moved set_headers function to separate file and created error_checker file

--- main.c
+++ main.c
@@ -3,22 +3,16 @@
 #include <string.h>
 #include <curl/curl.h>
 #include "config.h"
+#include "header_functions.h"
+#include "error_check.h"
 
 int main(int argc, char* argv[]) 
 {   
 
-    if(argc > 4)
-    {
-        fprintf(stderr, "\nUsage: %s <URL> <Output> [Arguments]\n", argv[0]);
-        fprintf(stderr, "Error 1 : Too many arguments passed\n");
-        return EXIT_FAILURE;
-    }
-    if(argc < 3)
-    {
-        fprintf(stderr, "\nUsage: %s <URL> <Output> [Arguments]\n", argv[0]);
-        fprintf(stderr, "Error 2 : Must include at least 3 arguments\n");
-        return EXIT_FAILURE;
-    }
+   if(argument_check(argc, argv) == EXIT_FAILURE)
+   {
+       return EXIT_FAILURE; 
+   }
 
     const char* URL = argv[1];
     const char* output = argv[2];
@@ -26,7 +20,7 @@ int main(int argc, char* argv[])
 
     if(!curl) 
     {
-        fprintf(stderr, "failed to initialize cURL.\n");
+        fprintf(stderr, "Error: Failed to initialize cURL.\n");
         return EXIT_FAILURE;
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,123 +37,18 @@ int main(int argc, char* argv[])
     // SET HEADERS vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     struct curl_slist *headers = NULL;
 
-    if(argc == 4 && strcmp(argv[3], HEADER_GET_SET_ARG) == 0) //GET HEADERS vvvvvvvvvv
+    if(argc == 4 && strcmp(argv[3], HEADER_GET_SET_ARG) == 0)
     {
-        printf("Custom Headers: Provide in order: User-Agent, Accept Format, Authorization Key >>\n");
-        char *_user_agent = NULL, *_accept_format = NULL, *_authorization_key = NULL;
-        size_t _user_agent_size = 128, _accept_format_size = 128, _authorization_key_size = 256;
-
-        //////////////////////////////////////////////////////
-        ////////////////// USER AGENT ////////////////////////
-        _user_agent = (char*)malloc(_user_agent_size);
-        if (_user_agent == NULL) 
-        {
-            fprintf(stderr, "Memory allocation failed for user agent!\n");
-            free(_user_agent);
-            fclose(fl);
-            curl_easy_cleanup(curl);
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Enter user agent (leave empty for default): ");
-        if (fgets(_user_agent, _user_agent_size, stdin) == NULL ||
-            strlen(_user_agent) == 1)
-        {
-            strcpy(_user_agent, HEADER_DEFAULT_USER_AGENT);
-        }
-        else _user_agent[strcspn(_user_agent, "\n")] = '\0';    
-        //////////////////////////////////////////////////////
-        ///////////////// ACCEPT FORMAT //////////////////////
-        _accept_format = (char*)malloc(_accept_format_size);
-        if (_accept_format == NULL) 
-        {
-            fprintf(stderr, "Memory allocation failed for accept format!\n");
-            free(_user_agent);
-            free(_accept_format);
-            fclose(fl);
-            curl_easy_cleanup(curl);
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Enter Accept Format (leave empty for default): ");
-        if (fgets(_accept_format, _accept_format_size, stdin) == NULL ||
-            strlen(_accept_format) == 1)
-        {
-            strcpy(_accept_format, HEADER_DEFAULT_ACCEPT);
-        }
-        else _accept_format[strcspn(_accept_format, "\n")] = '\0';
-        //////////////////////////////////////////////////////
-        ///////////////// AUTHORIZATION KEY //////////////////
-        _authorization_key = (char*)malloc(_authorization_key_size);
-        if (_authorization_key == NULL) 
-        {
-            fprintf(stderr, "Memory allocation failed for authorization key!\n");
-            free(_user_agent);
-            free(_accept_format);
-            free(_authorization_key);
-            fclose(fl);
-            curl_easy_cleanup(curl);
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Enter Authorization Key (leave empty for default): ");
-        if(fgets(_authorization_key, _authorization_key_size, stdin) == NULL 
-                || strlen(_authorization_key) == 1)
-        {
-            strcpy(_authorization_key, HEADER_DEFAULT_AUTHORIZATION);
-        }
-        else _authorization_key[strcspn(_authorization_key, "\n")] = '\0';
-
-
-        char _header_user_agent[256], _header_accept_format[256], _header_authorization_key[256];
-
-        snprintf(_header_user_agent, sizeof(_header_user_agent), "User-Agent: %s", _user_agent);
-        snprintf(_header_accept_format, sizeof(_header_accept_format), "Accept: %s", _accept_format);
-        snprintf(_header_authorization_key, sizeof(_header_authorization_key), "Authorization: %s", _authorization_key);
-
-        headers = curl_slist_append(headers, _header_user_agent);
-        headers = curl_slist_append(headers, _header_accept_format);
-        headers = curl_slist_append(headers, _header_authorization_key);
-
-        free(_user_agent);
-        free(_accept_format);
-        free(_authorization_key);
+        get_header_functions(argc, argv, &headers);
     }
-    else if(argc == 4 && strcmp(argv[3], HEADER_POST_SET_ARG) == 0) //POST HEADERS vvvvvvvvvv
-    {   
-        //////////////////////////////////////////////////////
-        //////////////////// CONTENT TYPE ////////////////////
-        printf("Custom Headers: Provide Content-Type (leave empty for default(JSON)) >>\n");
-        char* _content_type = NULL;
-        size_t _content_type_size = 128;
-        _content_type = (char*)malloc(_content_type_size);
-        if(_content_type == NULL)
-        {   
-            fprintf(stderr, "Memory allocation failed for Content-Type!\n");
-            free(_content_type);
-            fclose(fl);
-            curl_easy_cleanup(curl);
-            return EXIT_FAILURE;
-        }
-        if(fgets(_content_type, _content_type_size, stdin) == NULL
-            || strlen(_content_type) == 1 )
-        {
-            strcpy(_content_type, HEADER_DEFAULT_CONTENT_TYPE);
-        }
-        else _content_type[strcspn(_content_type, "\n")] = '\0';
 
-        char _header_content_type[256];
-        snprintf(_header_content_type, sizeof(_header_content_type), "Content-Type: %s", _content_type);
-
-        headers = curl_slist_append(headers, _header_content_type);
-
-        free(_content_type);
+    else if(argc == 4 && strcmp(argv[3], HEADER_POST_SET_ARG) == 0)
+    {
+        post_header_functions(argc, argv, &headers);
     }
     else
     {
-        headers = curl_slist_append(headers, HEADER_DEFAULT_USER_AGENT);
-        headers = curl_slist_append(headers, HEADER_DEFAULT_ACCEPT);
-        headers = curl_slist_append(headers, HEADER_DEFAULT_AUTHORIZATION);
+        default_header_functions(argc, argv, &headers);
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     // SET OPTIONS vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
